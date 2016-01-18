7d4c9e276f5679541789fcfc4564628726a8e1c3
Diana <dianajohnson13@gmail.com>
2016-01-17 17:22:21 -0800

    completed w2d1 rpn calculator

--- w2/w2d1/lib/00_rpn_calculator.rb
+++ w2/w2d1/lib/00_rpn_calculator.rb
@@ -1,7 +1,5 @@
 class RPNCalculator
-  attr_accessor :stack
-
-  OPERATIONS = [:+, :-]
+  attr_reader :stack
 
   def initialize
   	@stack = []
@@ -12,11 +10,72 @@ class RPNCalculator
   end
 
   def plus
-  	sum = stack[0] + stack[1]
+  	if stack.size < 2
+  		raise "calculator is empty"
+  	end
+  	num1 = stack.pop
+  	num2 = stack.pop
+  	stack << num1 + num2
+  end
+
+  def minus
+  	if stack.size < 2
+  		raise "calculator is empty"
+  	end
+  	num1 = stack.pop
+  	num2 = stack.pop
+  	stack << num2 - num1
+  end
+
+  def divide
+  	if stack.size < 2
+  		raise "calculator is empty"
+  	end
+  	num1 = stack.pop
+  	num2 = stack.pop
+  	stack << num2 / num1.to_f
+  end
+
+  def times
+  	if stack.size < 2
+  		raise "calculator is empty"
+  	end
+  	num1 = stack.pop
+  	num2 = stack.pop
+  	stack << num2 * num1.to_f
   end
 
   def value
-   stack.plus
+  	value = stack.pop
+  	stack << value
+  	value
+  end
+
+  def tokens(expression)
+  	operations = %w(+ - / *)
+  	elements = expression.split
+  	elements.map do |element|
+  		operations.include?(element) ? element.to_sym : element.to_i
+  	end
+  end
+
+  def evaluate(expression)
+  	toks = tokens(expression)
+  	toks.each do |token|
+  		case
+  		when token.is_a?(Integer)
+  			stack.push(token)
+  		when token == :+
+  			plus
+  		when token == :-
+  			minus
+  		when token == :/
+  			divide
+  		when token == :*
+  			times
+  		end
+  	end
+  	value
   end
 
 end
