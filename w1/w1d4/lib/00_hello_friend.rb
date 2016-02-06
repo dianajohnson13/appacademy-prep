3bae3df4409ba8d5011e1721b562229f07df8998
Diana <dianajohnson13@gmail.com>
2016-02-06 14:10:48 -0800

    added #intialize

--- w1/w1d4/lib/00_hello_friend.rb
+++ w1/w1d4/lib/00_hello_friend.rb
@@ -1,5 +1,11 @@
 class Friend
-  def greeting(who = nil)
-  	who == nil ? "Hello!" : "Hello, #{who}!"
+	attr_reader :name
+
+	def initialize(name = "Bob")
+		@name = name
+	end
+
+  def greeting(name = nil)
+  	name.nil? ? "Hello!" : "Hello, #{name}!"
   end
 end
